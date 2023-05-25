ce9608636fb5a3aa399dfa5cf17c8a89788bafcb
Gyanendra Yadav <gyanindrayadav1@gmail.com>
2023-05-25 17:03:53 +0530

    .

--- components/Explore.js
+++ components/Explore.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import { AiFillApple } from "react-icons/ai";
+import { AiFillApple, AiOutlineClose, AiOutlineCheck } from "react-icons/ai";
 import { FaGooglePlay } from "react-icons/fa";
 
 export const dataOne = [
@@ -110,7 +110,7 @@ const Explore = () => {
           </div>
         </div>
 
-        <div className="digonalBox2 text-white overflow-hidden bg-pink-700 -skew-y-12 py-10 lg:py-20">
+        <div className="digonalBox2 text-white overflow-hidden bg-pink-700 -skew-y-12 pt-10 pb-10 lg:pt-20 lg:pb-16">
           <div className="content2 p-10 lg:p-16 skew-y-12">
             <div className="flexTwo w-auto lg:w-1/2 flex flex-col justify-start items-start space-y-3 lg:space-y-5">
               <span className="text-sm lg:text-lg font-bold">
@@ -123,19 +123,161 @@ const Explore = () => {
             </div>
 
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 pt-5">
-              {
-                dataOne.map((one, i)=> {
-                  return (
-                    <div key={i} className="flex flex-col justify-start items-start space-y-1 bg-pink-600 rounded-lg p-4 lg:p-7">
-                      <span className="text-sm font-bold">{one.num}</span>
-                      <h2 className="text-xl lg:text-2xl pt-3">{one.headline}</h2>
-                      <p className="text-base">{one.des}</p>
-                    </div>
-                  )
-                })
-              }
+              {dataOne.map((one, i) => {
+                return (
+                  <div
+                    key={i}
+                    className="flex flex-col justify-start items-start space-y-1 bg-pink-600 rounded-lg p-4 lg:p-7"
+                  >
+                    <span className="text-sm font-bold">{one.num}</span>
+                    <h2 className="text-xl lg:text-2xl pt-3">{one.headline}</h2>
+                    <p className="text-base">{one.des}</p>
+                  </div>
+                );
+              })}
             </div>
           </div>
+        </div>
+
+        <div className="digonalBox3 text-white overflow-hidden bg-pink-700 -skew-y-12 py-10 lg:py-16">
+          <div className="content3 p-10 lg:p-16 skew-y-12">
+            <div className="flexTwo w-auto lg:w-1/2 flex flex-col justify-start items-start space-y-3 lg:space-y-5">
+              <span className="text-sm lg:text-lg font-bold">
+                THE INTEGRATIONS
+              </span>
+              <span className="text-3xl lg:text-5xl font-extrabold">
+                The Better Way
+              </span>
+              <hr className="h-1 w-12  bg-white" />
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 m-5 lg:m-10">
+              <Image
+                className="rounded-xl shadow-lg"
+                src="/five.png"
+                width={318}
+                height={685}
+                alt="five"
+              />
+
+              <Image
+                className="rounded-xl shadow-lg"
+                src="/four.png"
+                width={328}
+                height={695}
+                alt="four"
+              />
+
+              <Image
+                className="rounded-xl shadow-lg"
+                src="/six.png"
+                width={318}
+                height={685}
+                alt="six"
+              />
+            </div>
+          </div>
+        </div>
+
+        {/* #Review */}
+
+        <div className="bg-gradient-to-b from-pink-700  to-blue-600 py-10 lg:py-16 text-white -skew-y-12">
+          <div className="skew-y-12 flex flex-col lg:flex-row justify-center items-center lg:justify-evenly ">
+            <div className="flexOne flex flex-col justify-center items-center lg:justify-start lg:items-start space-y-2">
+              <span className="text-sm lg:text-lg font-bold">
+                NEW POSSIBILITIES
+              </span>
+              <span className="text-3xl lg:text-5xl font-extrabold pb-4">
+                Features
+              </span>
+              <hr className="h-1 w-12  bg-white" />
+            </div>
+
+            <div className="flexTwo flex flex-col justify-center items-center lg:justify-start lg:items-start space-y-2">
+              <span className="text-5xl font-extrabold lg:text-9xl">4.9</span>
+              <span className="text-xl font-extrabold lg:text-2xl">
+                out of 5
+              </span>
+            </div>
+          </div>
+        </div>
+
+
+        {/* #PLans */}
+        <div className="bg-gradient-to-r from-pink-700  to-blue-600 py-10 lg:py-16 text-white -skew-y-12">
+          <div className="skew-y-12 flex flex-col lg:flex-row justify-center items-center lg:justify-evenly ">
+            <div className="flexOne flex flex-col justify-center items-center lg:justify-start lg:items-start space-y-2">
+              <span className="text-sm lg:text-lg font-bold">
+              OUR PRICING PLANS
+              </span>
+              <span className="text-3xl lg:text-5xl font-extrabold pb-4">
+              Choose the best plan <br/> for your needs
+              </span>
+              <hr className="h-1 w-12  bg-white" />
+            </div>
+
+            <div className="flexTwo flex flex-col justify-center items-center lg:justify-start lg:items-start space-y-2">
+              <span className="text-5xl font-extrabold lg:text-9xl">1M</span>
+              <span className="text-xl font-extrabold lg:text-2xl">
+                Happy Users
+              </span>
+            </div>
+
+           
+
+            
+
+
+          </div>
+
+          <div className="skew-y-12 grid grid-cols-1 lg:grid-cols-3 gap-5 mx-5 lg:mx-20 border-2 border-gray-300 rounded-xl">
+              <div className="bg-red-600 rounded-xl p-5 flex flex-col justify-center items-center ">
+                <h3 className="text-3xl">PERSONAL</h3>
+                <span className="text-3xl">12 Dollar</span>
+                <div className="flex flex-col justify-center items-center">
+                  <div className="flex flex-col justify-start items-start">
+                    <div className="flex flex-row justify-start items-center">
+                      <span><AiOutlineClose/></span>
+                      <span>eCommerce Widgets</span>
+                    </div>
+                    <hr className="h-1 w-44"/>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+
+
+
+
+
+      </div>
+
+      <div className="newOne bg-blue-600 h-96 overflow-hidden ">
+        <div className="content -skew-y-12 p-16">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam,
+          excepturi? Temporibus nam suscipit officia ea consectetur modi, ipsum
+          fugiat ipsa reprehenderit dolorem maiores, nobis ipsam consequatur
+          animi voluptatem. Incidunt illo vel ut optio dolorem ad, sequi,
+          reprehenderit nobis aliquam numquam natus magni laborum quibusdam
+          corporis consequatur cupiditate officia dolores quas ipsum ipsam
+          possimus consequuntur molestias! Ratione recusandae id optio voluptate
+          laudantium iusto vel temporibus quasi in, adipisci architecto
+          asperiores eligendi aut dolorem nobis ipsum pariatur nisi obcaecati ad
+          ex voluptates accusamus quos. Eum, enim obcaecati, ducimus blanditiis
+          eius expedita sint corporis quos assumenda numquam similique ut
+          repudiandae iure adipisci vel quis pariatur commodi aperiam voluptas
+          eaque nesciunt. Culpa vel iste doloremque dolorum tenetur expedita rem
+          officia eaque libero eius placeat mollitia, atque fuga soluta minima
+          ratione numquam assumenda. Expedita cupiditate cum consectetur est
+          omnis tenetur repellendus nostrum autem ducimus, quae facilis sed
+          nulla eligendi illum. Repellat vero quibusdam mollitia, rerum rem
+          voluptatem dolorem est accusamus nulla quia nam velit sed at odit
+          consequatur totam dignissimos harum distinctio quo reprehenderit
+          excepturi. Nobis sed cum eveniet sint nisi labore ea cupiditate
+          tempora fugiat animi consectetur, quae nemo eligendi neque obcaecati
+          natus, atque repellendus quaerat minus vero alias beatae maxime
+          voluptatem. Harum, omnis!
         </div>
       </div>
     </>
--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,11 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Header = () => {
   return (
-    <div className='flex flex-row justify-center items-center py-5 bg-pink-600 text-3xl font-bold text-white'>AppPlus</div>
+    <div className='flex flex-row justify-center items-center py-5 bg-pink-600 '>
+      <Image src="/logo.png" width={150} height={50} alt="logo" />
+    </div>
   )
 }
 
